f7b194dfbd2e62756531d42126b38854e7664775
“KevinAlanTC” <323160698@comunidad.unam.mx>
2025-12-06 20:57:43 -0600

    Instanciación de sistema en Sudo

--- src/main/usuarios/Sudo.java
+++ src/main/usuarios/Sudo.java
@@ -1,12 +1,24 @@
 package usuarios;
 
-public class Sudo extends Usuario {
-    
-    public Sudo() {
+import utilidades.EntradaUtils;
+import java.util.Scanner;
+import principal.SistemaTareas;
+
+
+public class Sudo extends Usuario 
+{
+	private SistemaTareas sistema;
+    private Scanner scanner;
+
+    public Sudo() 
+    {
         super("SuperAdmin", "sudo@restaurante.com", "Sudo", "admin123"); // Contraseña por defecto
+        this.sistema = sistema;
+        this.scanner = scanner;
     }
     
-    public Administrador crearAdministrador(String nombre, String email, String password) {
+    public Administrador crearAdministrador(String nombre, String email, String password) 
+    {
         return new Administrador(nombre, email, password);
     }
 }
